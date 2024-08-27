c9269eb990b67269fcb5d31e966e73dd4140fe59
JohnH <john@symphonypc.com>
2024-08-27 08:24:10 +0200

    Updated Logo, added background to authenticated pages, looks tweak, redirect on logout

--- resources/js/Components/Mikrotik.jsx
+++ resources/js/Components/Mikrotik.jsx
@@ -18,14 +18,9 @@ export default function Mikrotik({ mikrotik }) {
 
     return (
         <div className="p-6 flex space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600 -scale-x-100" fill="none"
-                 viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round"
-                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
-            </svg>
             <div className="flex-1">
                 <div className="flex justify-between items-center">
-                    <div>
+                    <div className="">
                         <a href={'/mikrotik/'+mikrotik.id}>
                         <span className="text-gray-800">{mikrotik.user.name}</span>
                             <small
@@ -61,6 +56,7 @@ export default function Mikrotik({ mikrotik }) {
                     ? (<form onSubmit={submit}>
                         <input
                             type='text'
+                            id={'router_'+mikrotik.id}
                             value={data.router}
                             className="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                             onChange={e => setData('router', e.target.value)}
@@ -68,15 +64,17 @@ export default function Mikrotik({ mikrotik }) {
                         <InputError message={errors.router} className="mt-2"/>
                         <input
                             type='text'
+                            id={'publicKey_'+mikrotik.id}
                             value={data.publicKey}
                             className="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                            onChange={e => setData('router', e.target.value)}
+                            onChange={e => setData('publicKey', e.target.value)}
                         />
                         <InputError message={errors.publicKey} className="mt-2"/>
+                        <PrimaryButton className="mt-4">Update</PrimaryButton>
                     </form>)
                     : (
                         <a href={'/mikrotik/' + mikrotik.id}>
-                            <p className="mt-4 text-lg large-gray-900">{mikrotik.router}</p>
+                            <p className="mt-8 text-lg large-gray-900">{mikrotik.router}</p>
                             <p className="mt-4 text-lg large-gray-900">{mikrotik.publicKey}</p>
                         </a>
                     )}
